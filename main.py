f5271d941f81971a5425eb0f150be01beab89cf1
KostyaFun <para-pa-pam11@mail.ru>
2024-06-19 02:21:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+a= 'hello'
+print(len(a))
+first= 7
+second= 9
+print(first+second)
+print(first-second)
+summa= first+second
+print(summa)
+diff= first-second
+print(diff)
+first= 3
+second= 1
+third= 7
+print((first+second+third)/3)
+mean=(first+second+third)/3
+print(mean)
+first_string= "вторник"
+second_string= "понедельник"
+print(second_string + ', ' + first_string)
+a= 2
+b= 3
+c= 5
+f= (a*b)+(a*c)
+print((f**3)//2)
